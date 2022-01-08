cc99ca0aa655538435ce86c9d790d129582c33ed
Tony <robertanthony02@outlook.com>
2022-01-07 23:24:00 -0800

    First commit.

---
+++ main.cpp
@@ -0,0 +1,529 @@
+/******************************************************
+PARAMETERS
+******************************************************/
+static const float MOUSE_SPEED = 10.0f;
+static const bool REVERSE_MOUSE_BUTTONS = true;
+/******************************************************/
+
+#include <Windows.h>
+#include <iostream>
+#include <thread>       
+#include <chrono>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+#include <thread>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+struct mouse_state
+{
+    bool left_button_pressed;
+    bool right_button_pressed;
+};
+
+struct keyboard_state
+{
+    bool up_arrow_pressed;
+    bool down_arrow_pressed;
+    bool left_arrow_pressed;
+    bool right_arrow_pressed;
+    bool space_pressed;
+    bool enter_pressed;
+    bool win_pressed;
+};
+
+struct virtual_state
+{
+    bool minimize_pressed;
+    bool maximize_pressed;
+    bool select_all_pressed;
+    bool copy_pressed;
+    bool paste_pressed;
+};
+
+static bool g_should_quit = false;
+static bool g_is_enabled = false;
+static bool g_js_connected = false;
+static int g_js_id = -1;
+static mouse_state g_prev_ms_state;
+static keyboard_state g_prev_kb_state;
+static virtual_state g_prev_vir_state;
+
+using namespace std;
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "GL ERROR: %s\n", description);
+}
+
+bool check_joystick(int jid)
+{
+    if (glfwJoystickIsGamepad(jid))
+    {
+        auto name = string(glfwGetGamepadName(jid));
+        transform(name.begin(), name.end(), name.begin(), [](unsigned char c) { return tolower(c); });
+        auto found = name.find("gamepad");
+        if (found == string::npos) return false;
+        cout << "Gamepad connected [id=" << jid << ", name=" << glfwGetGamepadName(jid) << "]" << endl;
+        g_js_connected = true;
+        g_js_id = jid;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void check_joysticks()
+{
+    for (int i = 0; i < 5; i++) {
+        if (check_joystick(i)) return;
+    }
+}
+
+void joystick_callback(int jid, int event)
+{
+    if (event == GLFW_CONNECTED) {
+        if (!check_joystick(jid))
+            cout << "Joystick connected [id=" << jid << ", name=" << glfwGetGamepadName(jid) << "]" << endl;
+    } else if (event == GLFW_DISCONNECTED) {
+        if (g_js_connected && g_js_id == jid) {
+            cout << "Gamepad disconnected [id=" << jid << "]" << endl;
+            g_js_connected = false;
+            g_js_id = -1;
+        } else {
+            cout << "Joystick disconnected [id=" << jid << "]" << endl;
+        }
+    }
+}
+
+void left_mouse_button_pressed()
+{
+    INPUT mouse[1] = { 0 };
+    mouse[0].type = INPUT_MOUSE;
+    mouse[0].mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
+    SendInput(1, mouse, sizeof(INPUT));
+    g_prev_ms_state.left_button_pressed = true;
+}
+
+void left_mouse_button_released()
+{
+    INPUT mouse[1] = { 0 };
+    mouse[0].type = INPUT_MOUSE;
+    mouse[0].mi.dwFlags = MOUSEEVENTF_LEFTUP;
+    SendInput(1, mouse, sizeof(INPUT));
+    g_prev_ms_state.left_button_pressed = false;
+}
+
+void right_mouse_button_pressed()
+{
+    INPUT mouse[1] = { 0 };
+    mouse[0].type = INPUT_MOUSE;
+    mouse[0].mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
+    SendInput(1, mouse, sizeof(INPUT));
+    g_prev_ms_state.right_button_pressed = true;
+}
+
+void right_mouse_button_released()
+{
+    INPUT mouse[1] = { 0 };
+    mouse[0].type = INPUT_MOUSE;
+    mouse[0].mi.dwFlags = MOUSEEVENTF_RIGHTUP;
+    SendInput(1, mouse, sizeof(INPUT));
+    g_prev_ms_state.right_button_pressed = false;
+}
+
+void up_arrow_pressed()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_UP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.up_arrow_pressed = true;
+}
+
+void up_arrow_released()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_UP;
+    keyboard[0].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.up_arrow_pressed = false;
+}
+
+void down_arrow_pressed()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_DOWN;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.down_arrow_pressed = true;
+}
+
+void down_arrow_released()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_DOWN;
+    keyboard[0].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.down_arrow_pressed = false;
+}
+
+void left_arrow_pressed()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_LEFT;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.left_arrow_pressed = true;
+}
+
+void left_arrow_released()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_LEFT;
+    keyboard[0].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.left_arrow_pressed = false;
+}
+
+void right_arrow_pressed()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_RIGHT;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.right_arrow_pressed = true;
+}
+
+void right_arrow_released()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_RIGHT;
+    keyboard[0].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.right_arrow_pressed = false;
+}
+
+void space_pressed()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_SPACE;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.space_pressed = true;
+}
+
+void space_released()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_SPACE;
+    keyboard[0].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.space_pressed = false;
+}
+
+void enter_pressed()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_RETURN;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.enter_pressed = true;
+}
+
+void enter_released()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_RETURN;
+    keyboard[0].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.enter_pressed = false;
+}
+
+void win_pressed()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_LWIN;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.win_pressed = true;
+}
+
+void win_released()
+{
+    INPUT keyboard[1] = { 0 };
+    keyboard[0].type = INPUT_KEYBOARD;
+    keyboard[0].ki.wVk = VK_LWIN;
+    keyboard[0].ki.dwFlags = KEYEVENTF_KEYUP;
+    SendInput(1, keyboard, sizeof(INPUT));
+    g_prev_kb_state.win_pressed = false;
+}
+
+void minimize_pressed()
+{
+    g_prev_vir_state.minimize_pressed = true;
+}
+
+void minimize_released()
+{
+    POINT p;
+    GetCursorPos(&p);
+    auto hWnd = WindowFromPoint(p);
+    PostMessage(hWnd, WM_SYSCOMMAND, SC_MINIMIZE, 0);
+    g_prev_vir_state.minimize_pressed = false;
+}
+
+void maximize_pressed()
+{
+    g_prev_vir_state.maximize_pressed = true;
+}
+
+void maximize_released()
+{
+    POINT p;
+    GetCursorPos(&p);
+    auto hWnd = WindowFromPoint(p);
+    PostMessage(hWnd, WM_SYSCOMMAND, SC_MAXIMIZE, 0);
+    g_prev_vir_state.maximize_pressed = false;
+}
+
+void select_all_pressed()
+{
+    g_prev_vir_state.select_all_pressed = true;
+}
+
+void select_all_released()
+{
+    INPUT inputs[4] = {};
+    ZeroMemory(inputs, sizeof(inputs));
+
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_CONTROL;
+
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = 'A';
+
+    inputs[2].type = INPUT_KEYBOARD;
+    inputs[2].ki.wVk = 'A';
+    inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    inputs[3].type = INPUT_KEYBOARD;
+    inputs[3].ki.wVk = VK_CONTROL;
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    g_prev_vir_state.select_all_pressed = false;
+}
+
+void copy_pressed()
+{
+    g_prev_vir_state.copy_pressed = true;
+}
+
+void copy_released()
+{
+    INPUT inputs[4] = {};
+    ZeroMemory(inputs, sizeof(inputs));
+
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_CONTROL;
+
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = 'C';
+
+    inputs[2].type = INPUT_KEYBOARD;
+    inputs[2].ki.wVk = 'C';
+    inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    inputs[3].type = INPUT_KEYBOARD;
+    inputs[3].ki.wVk = VK_CONTROL;
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    g_prev_vir_state.copy_pressed = false;
+}
+
+void paste_pressed()
+{
+    g_prev_vir_state.paste_pressed = true;
+}
+
+void paste_released()
+{
+    INPUT inputs[4] = {};
+    ZeroMemory(inputs, sizeof(inputs));
+
+    inputs[0].type = INPUT_KEYBOARD;
+    inputs[0].ki.wVk = VK_CONTROL;
+
+    inputs[1].type = INPUT_KEYBOARD;
+    inputs[1].ki.wVk = 'V';
+
+    inputs[2].type = INPUT_KEYBOARD;
+    inputs[2].ki.wVk = 'V';
+    inputs[2].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    inputs[3].type = INPUT_KEYBOARD;
+    inputs[3].ki.wVk = VK_CONTROL;
+    inputs[3].ki.dwFlags = KEYEVENTF_KEYUP;
+
+    SendInput(ARRAYSIZE(inputs), inputs, sizeof(INPUT));
+    g_prev_vir_state.paste_pressed = false;
+}
+
+void check_keyboard_input()
+{
+    while (true) {
+        if (GetKeyState(VK_F12) < 0 && (GetKeyState(VK_LCONTROL) < 0 || GetKeyState(VK_RCONTROL) < 0)) {
+            g_is_enabled = true;
+            check_joysticks();
+            cout << "GamepadMouse has been enabled." << endl;
+            MessageBeep(MB_ICONASTERISK);
+        } else if (GetKeyState(VK_F11) < 0 && (GetKeyState(VK_LCONTROL) < 0 || GetKeyState(VK_RCONTROL) < 0)) {
+            g_is_enabled = false;
+            MessageBeep(MB_ICONERROR);
+            cout << "GamepadMouse has been disabled." << endl;
+        } else if (GetKeyState(VK_F10) < 0 && (GetKeyState(VK_LCONTROL) < 0 || GetKeyState(VK_RCONTROL) < 0)) {
+            ShowWindow(GetConsoleWindow(), SW_SHOW);
+        } else if (GetKeyState(VK_F9) < 0 && (GetKeyState(VK_LCONTROL) < 0 || GetKeyState(VK_RCONTROL) < 0)) {
+            g_should_quit = true;
+            return;
+        }
+
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+}
+
+int main()
+{
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+    if (!glfwInit())
+        cout << "Failed to initialize GLFW!" << endl;
+
+    glfwSetErrorCallback(error_callback);
+    glfwSetJoystickCallback(joystick_callback);
+
+    check_joysticks();
+
+    memset(&g_prev_ms_state, 0, sizeof(mouse_state));
+    memset(&g_prev_kb_state, 0, sizeof(keyboard_state));
+    memset(&g_prev_vir_state, 0, sizeof(virtual_state));
+
+    auto left_bumper = REVERSE_MOUSE_BUTTONS ? GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER : GLFW_GAMEPAD_BUTTON_LEFT_BUMPER;
+    auto right_bumper = REVERSE_MOUSE_BUTTONS ? GLFW_GAMEPAD_BUTTON_LEFT_BUMPER : GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER;
+
+    thread keyboard_input_thread(check_keyboard_input);
+    keyboard_input_thread.detach();
+
+    while(true) {
+        if (g_should_quit) break;
+
+        if (g_is_enabled) {
+            glfwPollEvents();
+
+            if (g_js_connected) {
+                GLFWgamepadstate state;
+
+                if (glfwGetGamepadState(0, &state)) {
+                    POINT cur_pos;
+                    if (GetCursorPos(&cur_pos))
+                    {
+                        auto x = state.axes[GLFW_GAMEPAD_AXIS_LEFT_X] + state.axes[GLFW_GAMEPAD_AXIS_RIGHT_X];
+                        auto y = state.axes[GLFW_GAMEPAD_AXIS_LEFT_Y] + state.axes[GLFW_GAMEPAD_AXIS_RIGHT_Y];
+                        if (abs(x) < 0.2) x = 0;
+                        if (abs(y) < 0.2) y = 0;
+                        auto x_velocity = x * MOUSE_SPEED;
+                        auto y_velocity = y * MOUSE_SPEED;
+                        SetCursorPos(cur_pos.x + x_velocity, cur_pos.y + y_velocity);
+                    }
+
+                    if (!g_prev_ms_state.left_button_pressed && state.buttons[left_bumper])
+                        left_mouse_button_pressed();
+                    else if (g_prev_ms_state.left_button_pressed && !state.buttons[left_bumper])
+                        left_mouse_button_released();
+
+                    if (!g_prev_ms_state.right_button_pressed && state.buttons[right_bumper])
+                        right_mouse_button_pressed();
+                    else if (g_prev_ms_state.right_button_pressed && !state.buttons[right_bumper])
+                        right_mouse_button_released();
+
+                    if (!g_prev_kb_state.up_arrow_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP])
+                        up_arrow_pressed();
+                    else if (g_prev_kb_state.up_arrow_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP])
+                        up_arrow_released();
+
+                    if (!g_prev_kb_state.down_arrow_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN])
+                        down_arrow_pressed();
+                    else if (g_prev_kb_state.down_arrow_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN])
+                        down_arrow_released();
+
+                    if (!g_prev_kb_state.left_arrow_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT])
+                        left_arrow_pressed();
+                    else if (g_prev_kb_state.left_arrow_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT])
+                        left_arrow_released();
+
+                    if (!g_prev_kb_state.right_arrow_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT])
+                        right_arrow_pressed();
+                    else if (g_prev_kb_state.right_arrow_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT])
+                        right_arrow_released();
+
+                    if (!g_prev_kb_state.space_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_A])
+                        space_pressed();
+                    else if (g_prev_kb_state.space_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_A])
+                        space_released();
+
+                    if (!g_prev_kb_state.enter_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_B])
+                        enter_pressed();
+                    else if (g_prev_kb_state.enter_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_B])
+                        enter_released();
+
+                    if (!g_prev_vir_state.minimize_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_X])
+                        minimize_pressed();
+                    else if (g_prev_vir_state.minimize_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_X])
+                        minimize_released();
+
+                    if (!g_prev_vir_state.maximize_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_Y])
+                        maximize_pressed();
+                    else if (g_prev_vir_state.maximize_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_Y])
+                        maximize_released();
+
+                    if (!g_prev_kb_state.win_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_START])
+                        win_pressed();
+                    else if (g_prev_kb_state.win_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_START])
+                        win_released();
+
+                    if (!g_prev_vir_state.select_all_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_BACK])
+                        select_all_pressed();
+                    else if (g_prev_vir_state.select_all_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_BACK])
+                        select_all_released();
+
+                    if (!g_prev_vir_state.copy_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_LEFT_THUMB])
+                        copy_pressed();
+                    else if (g_prev_vir_state.copy_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_LEFT_THUMB])
+                        copy_released();
+
+                    if (!g_prev_vir_state.paste_pressed && state.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_THUMB])
+                        paste_pressed();
+                    else if (g_prev_vir_state.paste_pressed && !state.buttons[GLFW_GAMEPAD_BUTTON_RIGHT_THUMB])
+                        paste_released();
+
+                    this_thread::sleep_for(chrono::milliseconds(5));
+                    continue;
+                }
+            }
+        }
+
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    
+    glfwTerminate();
+}
---
+++ readme.md
@@ -0,0 +1,61 @@
+# GamepadMouse
+
+## Overview
+
+Just a quick and dirty solution for using a gamepad for controlling the cursor and performing some common functions.  Currently only supports Windows, however it wouldn't take much effort to port to *nix (specifically, how it gets/sets the cursor position and how it sends simulated keystrokes).  
+
+## Configuration
+
+Currently there are just two parameters you can change at the top of `main.cpp`, which of course means configuration requires recompilation.  A future enhancement would include reading config data from a file.  
+
+## Building
+
+Microsoft Visual Studio 2019 was used to write/build this code, but of course you could use another compiler to compile `main.cpp`...just be sure to include `third_party/glfw/include` and link against `third_party/glfw/glfw3_mt.lib` (for static linkage) or one of the other included GLFW libraries.  
+
+## Running
+
+The app was designed to run in the background, consuming very little CPU time when not being used, so you could create a task using Task Scheduler that executes GamepadMouse.exe after logon and enable the service when you'd like to use it by sending it a command (details below).  
+
+Otherwise, just launch the executable whenever you want to use your gamepad to control the mouse, just remember that it starts up in a "disabled" state so you will still need to send it the command to enable itself.  
+
+## Usage
+
+When the app starts up, it does so in a "disabled" state, sleeping most of the time, waking up once a second to check the state of the keyboard to see if you are sending it a command.  There are four such commands:  
+
+**CTRL+F9** - Terminate the GamepadMouse.exe process.  
+
+**CTRL+F10** - Show the console window, which is helpful if you need to troubleshoot an issue.  
+
+**CTRL+F11** - Disable the service, meaning your gamepad will no longer control the mouse (important if you're playing a game!)
+
+**CTRL+F12** - Enable the service so that your gamepad will control the mouse and can be used to perform some common functions.  
+
+To be as efficient as possible, the app wakes up just once a second to check the keyboard state, so that means you may have to hold the combination of keys for up to one second before the app recognizes the command being sent.  You will hear a distinct sound for both the enable/disable commands and you can always show the console for visual confirmation.  
+
+## Gamepad Button Mapping
+
+**Left Bumper** -> Right mouse button.  Can be mapped to the left mouse button by changing the `REVERSE_MOUSE_BUTTONS` parameter at the top of `main.cpp`.
+
+**Right Bumper** -> Left mouse button.  Can be mapped to the right mouse button by changing the `REVERSE_MOUSE_BUTTONS` parameter at the top of `main.cpp`.
+
+**X Button** -> Minimize the window currently being hovered over by the mouse cursor.  
+
+**Y Button** -> Maximize the window currently being hovered over by the mouse cursor.  
+
+**A Button** -> Space key (which is often interpreted as an "OK" button in some applications).
+
+**B Button** -> Enter/return key.
+
+**Back Button** -> Select-all (CTRL+A).  
+
+**Left Thumbstick Button** -> Copy (CTRL+C).  
+
+**Right Thumbstick Button** -> Paste (CTRL+V).  
+
+**Start Button** -> Windows key (which brings up the Start menu).  
+
+Currently there is no way to change the mapping via a config file, but using the existing code as a guide it wouldn't be hard to change the mappings to your liking!
+
+## Cursor Movement
+
+You can use either the left or right thumbstick to move the mouse and the angle of both sticks will be added together when calculating the final velocity.  That means if you move both sticks in the same direction it will make the mouse move faster in that direction (so don't move them in opposite directions or else the mouse may not move at all!)  If the cursor moves too fast/slow, you can adjust its speed by changing the `MOUSE_SPEED` parameter at the top of `main.cpp`.  
